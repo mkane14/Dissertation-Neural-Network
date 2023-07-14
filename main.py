6fe491c1331ab27f491beccd2e8ab119771e1940
mkane14 <137116134+mkane14@users.noreply.github.com>
2023-07-14 14:27:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import function as func
 
-# Nboot value defines the number of initialisations of the network
-
 # Activation Function Options: [1.] tf.nn.tanh [2.] tf.nn.sigmoid [3.] tf.nn.elu [4.] tf.nn.relu [5.] tf.nn.selu [6.] tf.nn.gelu
 
-####    func.initialise_network(activation_function, width, depth, Nboot, STD, Mu)    ####
+####    func.initialise_network(activation_function, width, depth, Nboot, Cb, Cw, Mu, Nboot)    ####
 
-func.initialise_network(tf.nn.tanh, 10, 10, 1, 2, 0, 1000)
+func.initialise_network(activation_function=tf.nn.tanh, width=10, depth=10, Cb=1, Cw=2, Mu=0, Nboot=1000)
